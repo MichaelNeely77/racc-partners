7b6234bba41065e3449ae77d0de0fce7a6799dd4
Michael Neely <michael@michaelneely.net>
2024-05-27 12:56:06 -0400

    Second commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '7b48dc1993cea88e59c3');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '700e5e435a3a3f897be2');
